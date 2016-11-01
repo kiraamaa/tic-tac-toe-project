02f4b79a2f0a2a37c0d572fb83bf06a47b94dfa8
kiraamaa <kira.a.maclean@gmail.com>
2016-11-01 15:04:24 -0400

    Edit api.js files in master branch match gh-pages

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,37 +1,37 @@
 'use strict';
 
-const config = require('../config.js');
+const app = require('../app.js');
 
 const signUp = (data) =>
   $.ajax({
-    url: config.host + '/sign-up',
+    url: app.host + '/sign-up',
     method: 'POST',
     data,
   });
 
 const signIn = (data) =>
   $.ajax({
-    url: config.host + '/sign-in',
+    url: app.host + '/sign-in',
     method: 'POST',
     data,
   });
 
 const signOut = () =>
   $.ajax({
-    url: config.host + '/sign-out/' + store.user.id,
+    url: app.host + '/sign-out/' + app.user.id,
     method: 'DELETE',
     headers: {
-      Authorization: "Token token=" + store.user.token,
+      Authorization: "Token token=" + app.user.token,
     }
   });
 
 const changePassword = (data) =>
   $.ajax({
-    url: config.host + '/change-password/' + store.user.id,
+    url: app.host + '/change-password/' + app.user.id,
     method: 'PATCH',
     data,
     headers: {
-      Authorization: "Token token=" + store.user.token,
+      Authorization: "Token token=" + app.user.token,
     }
   });
 
