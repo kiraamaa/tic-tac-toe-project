3d67aeb5b86d07a53121e65dc8eea95336d69c51
kiraamaa <kira.a.maclean@gmail.com>
2016-11-01 14:31:16 -0400

    Re-organize files, begin using game api
    Remove redundant app.js file from auth
    Modify game/api.js

--- assets/scripts/auth/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const app = {
-  host: 'https://aqueous-atoll-85096.herokuapp.com'
-};
-
-
-module.exports = app;
--- assets/scripts/game/api.js
+++ assets/scripts/game/api.js
@@ -1,26 +1,42 @@
-// 'use strict';
-//
-// const app = require('../app');
-//
-//
-// const createGame = () => {
-//   $.ajax({
-//     url: app.host + '/games',
-//     method: 'POST',
-//     headers: {
-//       Authorization: 'Token token=' + app.user.token,
-//     },
-//   });
-// };
-//
-//
-//
-//
-//
-//
-//
-// module.exports = {
-//   createGame,
-//
-//
-// };
+'use strict';
+
+const app = require('../app');
+
+
+const createGame = () => {
+  let token = app.ser.token;
+  $.ajax({
+    url: app.host + '/games',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+  });
+};
+
+const getGame = () => {
+  $.ajax({
+        url: app.host + '/games',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    }
+  });
+};
+
+const updateWins = (data) => {
+  $.ajax({
+    url: app.host + '/games/' + app.game.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+    data: data,
+  });
+};
+
+module.exports = {
+  createGame,
+  updateWins,
+  getGame
+};
